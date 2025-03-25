4644a34f35211c98ccfb383df75b0bebd47a6e4c
yubinjodev <yubinjodev@gmail.com>
2025-03-25 23:19:21 +0900

    Feat: Configure middleware to redirect to rfp menu

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,10 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  const { pathname } = new URL(request.url);
-  if (pathname === "/") {
-    return NextResponse.redirect(new URL("/bids/favorites", request.url));
+  const { pathname } = new URL(request.url)
+  if (pathname === '/') {
+    return NextResponse.redirect(new URL('/bids/request-for-proposal', request.url))
   }
 
   //   return NextResponse.next();
