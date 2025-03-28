8d70fa5cfe57bb727d247dd21088e87f03735e64
yubinjodev <yubinjodev@gmail.com>
2025-03-28 22:51:37 +0900

    Fix: Add styles for rfp listing search tabs

--- src/features/rfp/components/rfp-search-tabs.tsx
+++ src/features/rfp/components/rfp-search-tabs.tsx
@@ -5,11 +5,30 @@ import { RfpSearchTabsCategories } from '../types/rfp-listing'
 export default function RfpSearchTabs() {
   const [currentSearchTab, setCurrentSearchTab] = useState<RfpSearchTabsCategories>('advanced')
 
+  const handleChangeSearchTabs = (tab: RfpSearchTabsCategories) => {
+    setCurrentSearchTab(tab)
+  }
+
   return (
     <Stack direction="row" spacing={1}>
-      <Chip label="간편 검색" />
-      <Chip label="AI 검색" />
-      <Chip label="고급 검색" />
+      <Chip
+        label="간편 검색"
+        variant={currentSearchTab === 'simple' ? 'filled' : 'outlined'}
+        color="success"
+        onClick={() => handleChangeSearchTabs('simple')}
+      />
+      <Chip
+        label="AI 검색"
+        variant={currentSearchTab === 'ai' ? 'filled' : 'outlined'}
+        color="error"
+        onClick={() => handleChangeSearchTabs('ai')}
+      />
+      <Chip
+        label="고급 검색"
+        variant={currentSearchTab === 'advanced' ? 'filled' : 'outlined'}
+        color="primary"
+        onClick={() => handleChangeSearchTabs('advanced')}
+      />
     </Stack>
   )
 }
