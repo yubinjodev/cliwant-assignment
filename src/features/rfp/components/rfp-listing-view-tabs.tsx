0ef25c957214674c8d88afaed9a8650f8f4c5f2b
yubinjodev <yubinjodev@gmail.com>
2025-03-28 22:43:57 +0900

    Chore: Apply rfp listing view tabs category constants

--- src/features/rfp/components/rfp-listing-view-tabs.tsx
+++ src/features/rfp/components/rfp-listing-view-tabs.tsx
@@ -1,19 +1,19 @@
 import { Tab, Tabs } from '@mui/material'
 import { SyntheticEvent, useState } from 'react'
+import { RFP_LISTING_VIEW_TABS_CATEGORY } from '../utils/constants/rfp-listing-view-tabs-category'
 
 export default function RfpListingViewTabs() {
-  const [value, setValue] = useState(0)
+  const [value, setValue] = useState('bidding-listing')
 
-  const handleChange = (_e: SyntheticEvent, newValue: number) => {
+  const handleChange = (_e: SyntheticEvent, newValue: string) => {
     setValue(newValue)
   }
 
   return (
     <Tabs value={value} onChange={handleChange}>
-      <Tab label="입찰 공고" />
-      <Tab label="사전 규격" />
-      <Tab label="발주 계획" />
-      <Tab label="유찰 공고" />
+      {RFP_LISTING_VIEW_TABS_CATEGORY.map((category) => (
+        <Tab label={category.label} key={category.value} value={category.value} />
+      ))}
     </Tabs>
   )
 }
---
+++ src/features/rfp/utils/constants/rfp-listing-view-tabs-category.ts
@@ -0,0 +1,18 @@
+export const RFP_LISTING_VIEW_TABS_CATEGORY = [
+  {
+    value: 'bidding-listing',
+    label: '입찰 공고',
+  },
+  {
+    value: 'preliminary-specification',
+    label: '사전 규격',
+  },
+  {
+    value: 'order-plan',
+    label: '발주 계획',
+  },
+  {
+    value: 'bidding-failure-listing',
+    label: '유찰 공고',
+  },
+]
