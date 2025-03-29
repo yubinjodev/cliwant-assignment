5ccbe13036ab638ff1f3501130869e3800e3330a
yubinjodev <yubinjodev@gmail.com>
2025-03-29 20:18:23 +0900

    Refactor: Separate saved rfp view tabs

---
+++ src/features/saved-rfp/components/saved-rfp-view-tabs.tsx
@@ -0,0 +1,9 @@
+import { Tab, Tabs } from '@mui/material'
+
+export default function SavedRfpViewTabs() {
+  return (
+    <Tabs value="none">
+      <Tab value="none" label="관심 공고" />
+    </Tabs>
+  )
+}
--- src/features/saved-rfp/components/saved-rfp.tsx
+++ src/features/saved-rfp/components/saved-rfp.tsx
@@ -1,15 +1,14 @@
 'use client'
 
-import { Paper, Stack, Tab, Tabs } from '@mui/material'
+import { Paper, Stack } from '@mui/material'
 import SavedRfpFilters from './saved-rfp-filters'
 import SavedRfpTable from './saved-rfp-table'
+import SavedRfpViewTabs from './saved-rfp-view-tabs'
 
 export default function SavedRfp() {
   return (
     <Stack spacing={2}>
-      <Tabs value="none">
-        <Tab value="none" label="관심 공고" />
-      </Tabs>
+      <SavedRfpViewTabs />
 
       <Stack sx={{ background: '#fff' }} component={Paper}>
         <SavedRfpFilters />
