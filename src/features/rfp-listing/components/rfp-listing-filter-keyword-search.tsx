344ebb577edd0f6c444617218111cfe37ff2cfcc
yubinjodev <yubinjodev@gmail.com>
2025-04-01 04:54:09 +0900

    Fix: Make rfp listing filter keyword search responsive

--- src/features/rfp-listing/components/rfp-listing-filter-keyword-search.tsx
+++ src/features/rfp-listing/components/rfp-listing-filter-keyword-search.tsx
@@ -28,48 +28,44 @@ export default function RfpListingFilterKeywordSearch(props: RfpFilterKeywordSea
   }
 
   return (
-    <Stack direction="row" spacing={1} alignItems="center" component="form" onSubmit={handleFormSubmit}>
-      <Select
-        size="small"
-        value={category}
-        onChange={(e) => handleChangeKeywordCategory(idx, e.target.value)}
-        sx={{ width: 320 }}
-      >
-        <MenuItem value="title">공고 제목</MenuItem>
-        <MenuItem value="body">첨부파일 본문</MenuItem>
-      </Select>
-      <Select
-        size="small"
-        value={condition}
-        onChange={(e) => handleChangeKeywordCondition(idx, e.target.value)}
-        sx={{ width: 200 }}
-      >
-        <MenuItem value="and">AND</MenuItem>
-        <MenuItem value="or">OR</MenuItem>
-      </Select>
-      <TextField
-        size="small"
-        placeholder="키워드를 입력해보세요"
-        value={keywordInput}
-        onChange={(e) => handleChangeKeywordInput(idx, e.target.value)}
-        sx={{
-          width: {
-            sm: '100%',
-          },
-        }}
-      />
-      <IconButton type="submit" disabled={!keywordInput}>
-        <AddIcon />
-      </IconButton>
-      <Stack direction="row" spacing={1} alignItems="center" sx={{ width: '100%', overflowX: 'auto' }}>
-        {selectedKeywords.map((keyword, keywordIdx) => (
-          <Chip
-            key={`selectedKeywords${keywordIdx}`}
-            label={keyword}
-            onDelete={() => handleChangeRemoveKeyword(idx, keywordIdx)}
+    <Stack direction={{ xs: 'column', md: 'row' }} spacing={1} component="form" onSubmit={handleFormSubmit}>
+      <Stack direction={{ xs: 'column', sm: 'row' }} spacing={1}>
+        <Stack direction="row" spacing={1}>
+          <Select size="small" value={category} onChange={(e) => handleChangeKeywordCategory(idx, e.target.value)}>
+            <MenuItem value="title">공고 제목</MenuItem>
+            <MenuItem value="body">첨부파일 본문</MenuItem>
+          </Select>
+          <Select size="small" value={condition} onChange={(e) => handleChangeKeywordCondition(idx, e.target.value)}>
+            <MenuItem value="and">AND</MenuItem>
+            <MenuItem value="or">OR</MenuItem>
+          </Select>
+        </Stack>
+
+        <Stack direction="row" spacing={1}>
+          <TextField
+            size="small"
+            placeholder="키워드를 입력해보세요"
+            value={keywordInput}
+            onChange={(e) => handleChangeKeywordInput(idx, e.target.value)}
+            sx={{ width: { md: 300 } }}
           />
-        ))}
+          <IconButton type="submit" disabled={!keywordInput}>
+            <AddIcon />
+          </IconButton>
+        </Stack>
       </Stack>
+
+      {selectedKeywords.length ? (
+        <Stack direction="row" spacing={1} alignItems="center" sx={{ width: '100%', overflowX: 'auto' }}>
+          {selectedKeywords.map((keyword, keywordIdx) => (
+            <Chip
+              key={`selectedKeywords${keywordIdx}`}
+              label={keyword}
+              onDelete={() => handleChangeRemoveKeyword(idx, keywordIdx)}
+            />
+          ))}
+        </Stack>
+      ) : null}
     </Stack>
   )
 }
